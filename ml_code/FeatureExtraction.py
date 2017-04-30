8ba5c7c931f08df688a52c2a4dffe007dff9163f
Siddharth Agarwal <Siddharth Agarwal>
2017-04-30 14:31:40 +0530

    Clean feature extraction module

--- ml_code/FeatureExtraction.py
+++ ml_code/FeatureExtraction.py
@@ -1,54 +1,70 @@
-def countVowels(string):
-    num_vowels=0
-    for char in string:
-        if char in "aeiou":
-           num_vowels = num_vowels+1
-    return num_vowels
+#Importing modules
 
-def countEI(string):
-    num_ei=0
-    for char in string:
-        if char in "ei":
-           num_ei = num_ei+1
-    return num_ei
+import pandas as pd
+import numpy as np
+import random
+from nltk import NaiveBayesClassifier,classify
+from sklearn.svm import LinearSVC
+from nltk.classify.scikitlearn import SklearnClassifier
 
-def _nameFeatures(name):
-    name=name.lower()
-    return{'name':name,
-        'lastChar':name[-1],
-          'lastTwoChar':name[-2:],
-          'isLastAEIY':(name[-1] in 'aeiy'),
-          'isSecLastAEI':(name[-2] in 'aei'),
-          'NoOfVowels':countVowels(name),
-           'NumEI':countEI(name),
-           'length':len(name),
-           'firstChar':name[0]
-           }
-def extractFeatures(dataframe):
-    featureSet = list()
-    for index,row in dataframe.iterrows():
-        featureSet.append((_nameFeatures(row['name']),row['gender']))
-    return featureSet
+class FeatureExtraction:
+"""
+Feature extraction module
+"""
+  def __init__(self, dataframe):
+      self.dataframe = dataframe
 
-def TrainAndTestNB(dataframe):
-    featureSet = extractFeatures(dataframe)
-    random.shuffle(featureSet)
-    name_count = len(featureSet)
-    cut = int(name_count*0.80)
-    trainSet = featureSet[:cut]
-    testSet = featureSet[cut:]
-    classifier = NaiveBayesClassifier.train(trainSet)
-    print('Testing Accuracy: {} '.format(classify.accuracy(classifier,testSet)))
-    print('Most Informative Features')
-    print(classifier.show_most_informative_features(5))
+  def countVowels(self, string):
+      num_vowels=0
+      for char in string:
+          if char in "aeiou":
+             num_vowels = num_vowels+1
+      return num_vowels
 
-def TrainAndTestSVM(dataframe):
-    featureSet = extractFeatures(dataframe)
-    random.shuffle(featureSet)
-    name_count = len(featureSet)
-    cut = int(name_count*0.80)
-    trainSet = featureSet[:cut]
-    testSet = featureSet[cut:]
-    classif = SklearnClassifier(LinearSVC())
-    classifier = classif.train(trainSet)
-    print('Testing Accuracy: {} '.format(classify.accuracy(classifier,testSet)))
+  def countEI(self, string):
+      num_ei=0
+      for char in string:
+          if char in "ei":
+             num_ei = num_ei+1
+      return num_ei
+
+  def _nameFeatures(self, name):
+      name=name.lower()
+      return{'name':name,
+          'lastChar':name[-1],
+            'lastTwoChar':name[-2:],
+            'isLastAEIY':(name[-1] in 'aeiy'),
+            'isSecLastAEI':(name[-2] in 'aei'),
+            'NoOfVowels':self.countVowels(name),
+             'NumEI':self.countEI(name),
+             'length':len(name),
+             'firstChar':name[0]
+             }
+  def extractFeatures(self):
+      self.featureSet = list()
+      for index,row in self.dataframe.iterrows():
+          self.featureSet.append((self._nameFeatures(row['name']),row['gender']))
+      return self.featureSet
+
+  def TrainAndTestNB(self):
+      self.featureSet = self.extractFeatures(self.dataframe)
+      random.shuffle(self.featureSet)
+      name_count = len(self.featureSet)
+      cut = int(name_count*0.80)
+      self.trainSet = self.featureSet[:cut]
+      self.testSet = self.featureSet[cut:]
+      self.classifier = NaiveBayesClassifier.train(self.trainSet)
+      print('Testing Accuracy: {} '.format(classify.accuracy(self.classifier,self.testSet)))
+      print('Most Informative Features')
+      print(self.classifier.show_most_informative_features(5))
+
+  def TrainAndTestSVM(self):
+      self.featureSet = self.extractFeatures(self.dataframe)
+      random.shuffle(self.featureSet)
+      name_count = len(self.featureSet)
+      cut = int(name_count*0.80)
+      self.trainSet = self.featureSet[:cut]
+      self.testSet = self.featureSet[cut:]
+      classif= SklearnClassifier(LinearSVC())
+      self.classifier = classif.train(self.trainSet)
+      print('Testing Accuracy: {} '.format(classify.accuracy(self.classifier,self.testSet)))
